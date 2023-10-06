54909e2d355cb98d50decf09b83eac20a6008abe
simon <simon.hobbs@electrooptical.net>
2023-10-06 12:39:41 -0400

    fixing package name

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
 
-setup(name='tds7xx',
+with open("README.md", 'r') as f:
+    description = f.read()
+
+
+with open("requirements.txt", 'r') as f:
+    requirements = f.readlines()
+
+
+setup(name='tdsxx4a',
       version='1.0.0',
-      description='HP TDS4xx Series GPIB Driver',
+      description='HP TDS4xx4a Series GPIB Driver',
+      long_description = description,
       url='',
       author='ElectroOptical Innovations, LLC.',
       author_email='simon.hobbs@electrooptical.net',
       license='BSD',
-      packages=[],
-      #packages=find_packages(),
-      install_requires=[
-          'prologix-gpib-ethernet',
-          'click',
-          'pyserial',
-          'numpy',
-          'timeout_decorator'
-      ],
+      packages=find_packages(),
+      install_requires=requirements,
       test_suite='nose.collector',
       tests_require=['nose'],
       scripts=["bin/tektronix-tds-7xx.py"],
